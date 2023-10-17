6c446acdab1a409a91f30d1bf3be36839611a679
Dan <dn.rd96@outlook.com>
2023-10-17 14:39:51 +0100

    Add led manager for arcade button clicks

---
+++ src/led_manager.py
@@ -0,0 +1,22 @@
+from machine import Pin, PWM
+import utime
+
+# Function to make the LED blink as an alert
+def led_alert():
+    led = PWM(Pin(16))
+    led.freq(1000)
+    min_value = 25
+    max_value = 100
+    sleep_length = 50
+    led_duty_cycle = 500
+    led_value = 50
+    original_led_speed = 5
+    led_speed = original_led_speed
+    
+    # Loop to make the LED brightness go up and down
+    for _ in range(6):
+        led_value += led_speed
+        led.duty_u16(int(led_value * led_duty_cycle))
+        utime.sleep_ms(sleep_length)
+        if led_value >= max_value or led_value <= min_value:
+            led_speed = -led_speed
