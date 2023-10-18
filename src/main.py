24e1ad6122bb201ddb35c7b4a8297c2515eebc10
Dan <dn.rd96@outlook.com>
2023-10-18 09:54:43 +0100

    Update main to ensure serving number starts as one

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ while True:
         led_alert()  # Activate LED alert
         ticket_number += 1
         if ticket_number > 999:
-            ticket_number = 0
+            ticket_number = 1
         print_ticket(ticket_number)
         save_state(ticket_number, serving_number)  # Save the updated state
         time.sleep(0.5)
@@ -37,7 +37,7 @@ while True:
     if button_1.value():
         serving_number += 1
         if serving_number > 999:
-            serving_number = 0
+            serving_number = 1
         display.update(serving_number)
         save_state(ticket_number, serving_number)
         time.sleep(0.5)
@@ -45,7 +45,7 @@ while True:
     # If button 2 is pressed, decrement the serving number and update the display
     if button_2.value():
         serving_number -= 1
-        if serving_number < 0:
+        if serving_number < 1:
             serving_number = 999
         display.update(serving_number)
         save_state(ticket_number, serving_number)
@@ -54,7 +54,7 @@ while True:
     # If button 3 is pressed, reset both the ticket and serving numbers
     if button_3.value():
         ticket_number = 0
-        serving_number = 0
+        serving_number = 1
         display.update(serving_number)
         save_state(ticket_number, serving_number)
         time.sleep(0.5)
