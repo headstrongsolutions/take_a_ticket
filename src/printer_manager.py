bd2bf565a744a6964a7070fd1184eb99c86e534d
Dan <dn.rd96@outlook.com>
2023-10-18 13:26:29 +0100

    fix indents

--- src/printer_manager.py
+++ src/printer_manager.py
@@ -3,36 +3,29 @@ from Adafruit_Thermal import *
 # Function to print a ticket with a specific number
 def print_ticket(ticket_number):
      printer = Adafruit_Thermal()
-    
-    # Initialize printer settings
-    printer.wake()
-    printer.setDefault()
-    printer.justify('C')
-    
-    # Header: "Please Take Ticket"
-    printer.setSize('M')  # Medium size
-    printer.boldOn()  # Bold text for emphasis
-    printer.println("Please Take Ticket")
-    printer.boldOff()
-    
-    # Space
-    printer.println("")
-    
-    # Ticket Number
-    printer.setSize('L')  # Large size
-    printer.doubleHeightOn()  # Make text double height
-    printer.boldOn()  # Bold text for emphasis
-    printer.println(f"{ticket_number:03}")
-    printer.boldOff()
-    printer.doubleHeightOff()
-    
-    # Space
-    printer.println("")
-    
-    # Footer: "wait for your number"
-    printer.setSize('M')  # Medium size
-    printer.println("wait for your number")
-    
-    # Finalize
-    printer.feed(3)  # Feed 3 lines for spacing
-    printer.sleep()  # Save some power
+     # Initialize printer settings
+     printer.wake()
+     printer.setDefault()
+     printer.justify('C')
+     # Header: "Please Take Ticket"
+     printer.setSize('M')  # Medium size
+     printer.boldOn()  # Bold text for emphasis
+     printer.println("Please Take Ticket")
+     printer.boldOff()
+     # Space
+     printer.println("")
+     # Ticket Number
+     printer.setSize('L')  # Large size
+     printer.doubleHeightOn()  # Make text double height
+     printer.boldOn()  # Bold text for emphasis
+     printer.println(f"{ticket_number:03}")
+     printer.boldOff()
+     printer.doubleHeightOff()
+     # Space
+     printer.println("")
+     # Footer: "wait for your number"
+     printer.setSize('M')  # Medium size
+     printer.println("wait for your number")
+     # Finalize
+     printer.feed(3)  # Feed 3 lines for spacing
+     printer.sleep()  # Save some power
