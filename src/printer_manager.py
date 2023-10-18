5acac73e74b3cde0a7fe5785a1cd6b6b3cb0e58e
Dan <dn.rd96@outlook.com>
2023-10-18 11:49:04 +0100

    update printer_manager

--- src/printer_manager.py
+++ src/printer_manager.py
@@ -3,24 +3,36 @@ from Adafruit_Thermal import *
 # Function to print a ticket with a specific number
 def print_ticket(ticket_number):
      printer = Adafruit_Thermal()
-     printer.println("")
-     printer.inverseOff()
-     printer.justify('C')
-     printer.setSize('S')
-     printer.println("################################")
-     printer.println("")
-     printer.println("Take your ticket")
-     printer.println("")
-     printer.setSize('L')
-     printer.doubleHeightOn()
-     printer.boldOn()
-     printer.print(f"{ticket_number:03}")
-     printer.println("")
-     printer.boldOff()
-     printer.doubleHeightOff()
-     printer.setSize('S')
-     printer.println("")
-     printer.println("Wait till your tickets turn")
-     printer.println("")
-     printer.println("################################")
-     printer.feed(3)
+    
+    # Initialize printer settings
+    printer.wake()
+    printer.setDefault()
+    printer.justify('C')
+    
+    # Header: "Please Take Ticket"
+    printer.setSize('M')  # Medium size
+    printer.boldOn()  # Bold text for emphasis
+    printer.println("Please Take Ticket")
+    printer.boldOff()
+    
+    # Space
+    printer.println("")
+    
+    # Ticket Number
+    printer.setSize('L')  # Large size
+    printer.doubleHeightOn()  # Make text double height
+    printer.boldOn()  # Bold text for emphasis
+    printer.println(f"{ticket_number:03}")
+    printer.boldOff()
+    printer.doubleHeightOff()
+    
+    # Space
+    printer.println("")
+    
+    # Footer: "wait for your number"
+    printer.setSize('M')  # Medium size
+    printer.println("wait for your number")
+    
+    # Finalize
+    printer.feed(3)  # Feed 3 lines for spacing
+    printer.sleep()  # Save some power
