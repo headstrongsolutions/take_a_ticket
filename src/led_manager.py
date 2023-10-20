06e5da5b020e5d5a6d817c594bc0b0b45693b896
Dan <dn.rd96@outlook.com>
2023-10-20 11:10:06 +0100

    Update led manager to while printing cycle led

--- src/led_manager.py
+++ src/led_manager.py
@@ -1,5 +1,6 @@
 from machine import Pin, PWM
 import utime
+from printer_manager import is_printing
 
 # Function to make the LED blink as an alert
 def led_alert():
@@ -12,11 +13,14 @@ def led_alert():
     led_value = 50
     original_led_speed = 5
     led_speed = original_led_speed
-    
+
     # Loop to make the LED brightness go up and down
-    for _ in range(6):
+    while is_printing():
         led_value += led_speed
         led.duty_u16(int(led_value * led_duty_cycle))
         utime.sleep_ms(sleep_length)
         if led_value >= max_value or led_value <= min_value:
             led_speed = -led_speed
+
+    # Turn off the LED once printing is complete
+    led.duty_u16(0)
