eaec4d43450a2a6d1f922d133a9faf6f810ff4c8
Dan <dn.rd96@outlook.com>
2023-10-20 11:10:33 +0100

    Update print manager to show flag if printing

--- src/printer_manager.py
+++ src/printer_manager.py
@@ -1,8 +1,18 @@
+# Import the necessary libraries
 from Adafruit_Thermal import *
+import utime
+
+# Set printing flag to false
+printing_flag = False
+
+def set_printing_flag(value):
+     global printing_flag
+     printing_flag = value
 
 # Function to print a ticket with a specific number
 def print_ticket(ticket_number):
      printer = Adafruit_Thermal()
+
      # Initialize printer settings
      printer.wake()
      printer.setDefault()
@@ -10,7 +20,7 @@ def print_ticket(ticket_number):
      # Header: "Please Take Ticket"
      printer.setSize('M')  # Medium size
      printer.boldOn()  # Bold text for emphasis
-     printer.println("################################")
+     printer.println("###############################")
      printer.println("")
      printer.println("Please Take Ticket")
      printer.boldOff()
@@ -30,8 +40,13 @@ def print_ticket(ticket_number):
      printer.boldOn()  # Bold text for emphasis
      printer.println("wait for your tickets turn.")
      printer.println("")
-     printer.println("################################")
+     printer.println("###############################")
      printer.boldOff()
      # Finalize
      printer.feed(3)  # Feed 3 lines for spacing
      printer.sleep()  # Save some power
+     utime.sleep(5)
+
+# Fucntion to return if the printer is currently printing
+def is_printing():
+     return printing_flag
